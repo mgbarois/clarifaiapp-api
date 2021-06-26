948da9fe42691b6da205ef1f3caf01ce5137e13c
mgbarois <mgbarois@gmail.com>
2021-06-26 15:29:28 +0200

    Connected Heroku db

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-angular-04088', // 127.0.0.1
         user: 'postgres',
         password: 'postgres',
         database: 'clarifai'
